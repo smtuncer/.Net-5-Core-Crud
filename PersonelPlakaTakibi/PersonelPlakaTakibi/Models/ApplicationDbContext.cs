102264c52bb7ce5c48be5a933f8126df28107951
Selçuk Mehmet TUNÇER <70010594+smtuncer@users.noreply.github.com>
2022-05-21 14:01:21 +0300

    Update ApplicationDbContext.cs

--- PersonelPlakaTakibi/PersonelPlakaTakibi/Models/ApplicationDbContext.cs
+++ PersonelPlakaTakibi/PersonelPlakaTakibi/Models/ApplicationDbContext.cs
@@ -6,7 +6,7 @@ namespace PersonelPlakaTakibi.Models
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"workstation id=PersonelPlakaTakipDb.mssql.somee.com;packet size=4096;user id=smtuncer123_SQLLogin_1;pwd=zz5qm88typ;data source=PersonelPlakaTakipDb.mssql.somee.com;persist security info=False;initial catalog=PersonelPlakaTakipDb");
+            optionsBuilder.UseSqlServer(@".......");
         }
         public DbSet<Admin> Admins { get; set; }
         public DbSet<Personel> Personels { get; set; }
